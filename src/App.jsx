73cde271bba86abfb40b9f280ccf620828814387
Omkar Thakre <omkar01thakre@gmail.com>
2025-06-07 11:39:58 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Navbar from '../Component/Navbar';
-import Footer from '../Component/Footer';
-
-import Home from '../MainComponent/Home';
-import About from '../MainComponent/About';
-import Recipes from '../MainComponent/Recipes';
-import Contact from '../MainComponent/Contact';
-import Spices from '../MainComponent/Spices';
-import MasalaMixes from '../MainComponent/MasalaMixes';
-import SpecialtyPacks from '../MainComponent/SpecialtyPacks';
-
+import React from 'react'
+import Navbar from '../Component/Navbar'
+import Hero from '../Component/Hero'
+import Footer from '../Component/Footer'
+import MainCards from '../Component/MainCards'
+import SignatureCollections from '../Component/SignatureCollections'
+import BestSellers from '../Component/bestsellers'
+import LegacySection from '../Component/LegacySection'
+import Testimonials from '../Component/Testimonials'
+import AutoscrollSlider from '../Component/AutoScrollSlider'
+import SubscribeSection from '../Component/SubscribeSection'
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/recipes" element={<Recipes />} />
-        <Route path="/contact" element={<Contact />} /> */}
-        {/* <Route path="/collections/spices" element={<Spices />} />
-        <Route path="/collections/masala-mixes" element={<MasalaMixes />} />
-        <Route path="/collections/specialty-packs" element={<SpecialtyPacks />} /> */}
-      </Routes>
-      <Footer />
-    </Router>
-  );
-};
+    <div>
+      <Navbar/>
+      <Hero/>
+      <SignatureCollections/>
+      <BestSellers/>
+      <LegacySection/>
+      <Testimonials/>
+      <AutoscrollSlider/>
+      <SubscribeSection/>
+      <Footer/>
+    </div>
+  )
+}
 
-export default App;
+export default App
